0a7c00a31908341d7577365c8ffc157c044942bb
Mark Ryan <markusdryan@gmail.com>
2019-09-30 22:59:42 +0200

    Optimize ARM memset little
    We now use STM is there are more than 8 words to write.
    
    Signed-off-by: Mark Ryan <markusdryan@gmail.com>

--- arch/arm32/arm_mem.c
+++ arch/arm32/arm_mem.c
@@ -25,13 +25,16 @@ void subtilis_arm_mem_memseti32(subtilis_ir_section_t *s,
 	const size_t base_reg = 0;
 	const size_t size = 1;
 	const size_t val = 2;
-	const size_t end_reg = 10;
+	const size_t stm_end_reg = 10;
+	const size_t end_reg = 11;
 	subtilis_arm_instr_t *instr;
 	subtilis_arm_stran_instr_t *stran;
 	subtilis_arm_br_instr_t *br;
 	subtilis_arm_data_instr_t *datai;
+	size_t i;
 	size_t end_label = arm_s->label_counter++;
 	size_t start_label = arm_s->label_counter++;
+	size_t start_small_label = arm_s->label_counter++;
 
 	instr =
 	    subtilis_arm_section_add_instr(arm_s, SUBTILIS_ARM_INSTR_ADD, err);
@@ -46,7 +49,61 @@ void subtilis_arm_mem_memseti32(subtilis_ir_section_t *s,
 	datai->op2.type = SUBTILIS_ARM_OP2_REG;
 	datai->op2.op.reg = size;
 
+	subtilis_arm_add_sub_imm(arm_s, SUBTILIS_ARM_CCODE_AL, false,
+				 stm_end_reg, end_reg, 8, err);
+	if (err->type != SUBTILIS_ERROR_OK)
+		return;
+
+	subtilis_arm_add_cmp(arm_s, SUBTILIS_ARM_INSTR_CMP,
+			     SUBTILIS_ARM_CCODE_AL, base_reg, stm_end_reg, err);
+	if (err->type != SUBTILIS_ERROR_OK)
+		return;
+
+	instr =
+	    subtilis_arm_section_add_instr(arm_s, SUBTILIS_ARM_INSTR_B, err);
+	if (err->type != SUBTILIS_ERROR_OK)
+		return;
+
+	br = &instr->operands.br;
+	br->ccode = SUBTILIS_ARM_CCODE_GT;
+	br->link = false;
+	br->link_type = SUBTILIS_ARM_BR_LINK_VOID;
+	br->target.label = start_small_label;
+
+	for (i = 3; i <= 9; i++) {
+		subtilis_arm_add_mov_reg(arm_s, SUBTILIS_ARM_CCODE_AL, false, i,
+					 2, err);
+		if (err->type != SUBTILIS_ERROR_OK)
+			return;
+	}
+
 	subtilis_arm_section_add_label(arm_s, start_label, err);
+	if (err->type != SUBTILIS_ERROR_OK)
+		return;
+
+	subtilis_arm_add_mtran(arm_s, SUBTILIS_ARM_INSTR_STM,
+			       SUBTILIS_ARM_CCODE_AL, 0, 255 << 2,
+			       SUBTILIS_ARM_MTRAN_IA, true, err);
+	if (err->type != SUBTILIS_ERROR_OK)
+		return;
+
+	subtilis_arm_add_cmp(arm_s, SUBTILIS_ARM_INSTR_CMP,
+			     SUBTILIS_ARM_CCODE_AL, base_reg, stm_end_reg, err);
+	if (err->type != SUBTILIS_ERROR_OK)
+		return;
+
+	instr =
+	    subtilis_arm_section_add_instr(arm_s, SUBTILIS_ARM_INSTR_B, err);
+	if (err->type != SUBTILIS_ERROR_OK)
+		return;
+
+	br = &instr->operands.br;
+	br->ccode = SUBTILIS_ARM_CCODE_LT;
+	br->link = false;
+	br->link_type = SUBTILIS_ARM_BR_LINK_VOID;
+	br->target.label = start_label;
+
+	subtilis_arm_section_add_label(arm_s, start_small_label, err);
 	if (err->type != SUBTILIS_ERROR_OK)
 		return;
 
@@ -90,7 +147,7 @@ void subtilis_arm_mem_memseti32(subtilis_ir_section_t *s,
 	br->ccode = SUBTILIS_ARM_CCODE_AL;
 	br->link = false;
 	br->link_type = SUBTILIS_ARM_BR_LINK_VOID;
-	br->target.label = start_label;
+	br->target.label = start_small_label;
 
 	subtilis_arm_section_add_label(arm_s, end_label, err);
 	if (err->type != SUBTILIS_ERROR_OK)
